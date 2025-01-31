6813ed870a2684b3a0060027527b3eca8ef36b9a
Cpl-flowers <126724988+Cpl-flowers@users.noreply.github.com>
2025-01-31 23:47:04 +0000

    need to set point

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,24 @@
+public class BankAccount {
 
+    
+
+    public static void main(String[] args) {
+
+        float account = (float) 2175.37;
+        System.out.println(account);
+        account -= 302.50;
+        System.out.println(account);
+        account += 50.03;
+        System.out.println(account);
+        account = (account / 2) + 20;
+        System.out.println(account);
+        --account;
+        System.out.println(account);
+        account = account * 2;
+        System.out.println(account);
+        ++account;
+        
+
+        System.out.println("Bob's new balance: " + account);
+    }
+}

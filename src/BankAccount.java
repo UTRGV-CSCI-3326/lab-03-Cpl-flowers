fa3477212bf81cbf537b287f4d9d8740bbc18c61
Cpl-flowers <126724988+Cpl-flowers@users.noreply.github.com>
2025-02-02 00:40:50 +0000

    Set Precision added

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,20 +5,20 @@ public class BankAccount {
     public static void main(String[] args) {
 
         float account = (float) 2175.37;
-        System.out.println(account);
+        //System.out.println(account);
         account -= 302.50;
-        System.out.println(account);
+        //System.out.println(account);
         account += 50.03;
-        System.out.println(account);
+        //System.out.println(account);
         account = (account / 2) + 20;
-        System.out.println(account);
+        //System.out.println(account);
         --account;
-        System.out.println(account);
+        //System.out.println(account);
         account = account * 2;
-        System.out.println(account);
+        //System.out.println(account);
         ++account;
         
 
-        System.out.println("Bob's new balance: " + account);
+        System.out.println("Bob's new balance: " + String.format("%.2f", account));
     }
 }
